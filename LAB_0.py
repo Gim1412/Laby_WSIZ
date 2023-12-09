a83414e769bd7c0b0307a91ff81445ab2eb655fe
GIM1412 <w70568@student.wsiz.edu.pl>
2023-12-09 20:08:42 +0100

    wersja1

--- LAB_0.py
+++ LAB_0.py
@@ -1,15 +1,4 @@
-print("Koszty Podróży")
-
-x=int(input("podaj długość podróży w kilometrach :"))
-
-print("Długość podróży : ",x,"km")
-
-y=int(input("podaj średnie spalanie (w litrach na 100 km) :"))
-print("Średnie spalanie :",y,"l/100km")
-
-print("Przewidywane zyżycie paliwa : ",y/100*x,"  Przewidywany koszt podróży : ",y/100*x*6.5,"zł")
-
-print("Program2")
+print("Program1")
 
 print("Pole prostokąta = x*y")
 print("Obwód prostokąta = 2x+2y")
@@ -22,6 +11,29 @@ print("y =", y)
 print("Pole prostokąta x*y = ",x*y)
 
 print("Obwód prostokąta 2x + 2y = ",2*x+2*y)
+
+
+print("Program2")
+
+print("Koszty Podróży")
+
+import random
+los = random.randint(1, 100000)
+
+
+print("Długość podróży : ", los, "km")
+
+y = int(input("podaj średnie spalanie (w litrach na 100 km) :"))
+print("Średnie spalanie :", y, "l/100km")
+
+zużycie = round(y/100*los, 2)
+
+koszty = round(y/100*los*6.5, 2)
+
+zdanie = f'Przewidywane zyżycie paliwa : {zużycie} l"  Przewidywany koszt podróży : {koszty} zł'
+
+print(zdanie)
+
 
 
 
