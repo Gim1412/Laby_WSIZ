140a37a3d551e327f1d45190b292395b14df6c12
GIM1412 <w70568@student.wsiz.edu.pl>
2023-12-09 18:39:16 +0100

    wersja1

---
+++ LAB_0.py
@@ -0,0 +1,27 @@
+print("Koszty Podróży")
+
+x=int(input("podaj długość podróży w kilometrach :"))
+
+print("Długość podróży : ",x,"km")
+
+y=int(input("podaj średnie spalanie (w litrach na 100 km) :"))
+print("Średnie spalanie :",y,"l/100km")
+
+print("Przewidywane zyżycie paliwa : ",y/100*x,"  Przewidywany koszt podróży : ",y/100*x*6.5,"zł")
+
+print("Program2")
+
+print("Pole prostokąta = x*y")
+print("Obwód prostokąta = 2x+2y")
+
+x=int(input("podaj długość boku x:"))
+print("x =", x)
+y=int(input("podaj długość boku y:"))
+print("y =", y)
+
+print("Pole prostokąta x*y = ",x*y)
+
+print("Obwód prostokąta 2x + 2y = ",2*x+2*y)
+
+
+
