4686da86ed54ef6265c3292f8bcf91447a898c51
GIM1412 <w70568@student.wsiz.edu.pl>
2023-12-10 17:48:06 +0100

    wersja1

--- LaB_1.py
+++ LaB_1.py
@@ -8,6 +8,38 @@ elif x <= 18:
     print("Cena biletu: 10zł")
 else:
     print("Cena biletu: 20zł")
-
+# Zadanie 2
 print("Zadanie 2.")
+
+print("Wielkość liter")
+
+x=input("Wpisz dowolną literę:")
+if x.isupper():
+    print("Wielka litera")
+elif x.islower():
+    print("Mała litera")
+else:
+    print("Spróbuj ponownie")
+
+ # Zadanie 3
+
+print("Kalkulator")
+
+x = int(input("Podaj pierwszą liczbę:"))
+y = int(input("Podaj drugą liczbę:"))
+
+print("x =", x)
+print("y =", y)
+
+print("1. Dodawanie")
+print("2. Odejmowanie")
+print("3. Mnożenie")
+print("4. Dzielenie")
+
+z = int(input("Podaj numer operacji:"))
+
+if
+
+
+
 
