5908dcf46e90a9f2fef12d83d03cf47a607d5cd1
GIM1412 <w70568@student.wsiz.edu.pl>
2023-12-10 09:14:26 +0100

    wersja1

--- LAB_0.py
+++ LAB_0.py
@@ -3,14 +3,14 @@ print("Program1")
 print("Pole prostokąta = x*y")
 print("Obwód prostokąta = 2x+2y")
 
-x=int(input("podaj długość boku x:"))
+x = int(input("podaj długość boku x:"))
 print("x =", x)
-y=int(input("podaj długość boku y:"))
+y = int(input("podaj długość boku y:"))
 print("y =", y)
 
-print("Pole prostokąta x*y = ",x*y)
+print("Pole prostokąta x*y = ", x*y)
 
-print("Obwód prostokąta 2x + 2y = ",2*x+2*y)
+print("Obwód prostokąta 2x + 2y = ", 2*x+2*y)
 
 
 print("Program2")
---
+++ LaB_1.py
@@ -0,0 +1,13 @@
+print("Zadanie 1. Cena biletu")
+
+x = int(input("Podaj swój wiek:"))
+print("Wiek:", x, "lat(a)")
+if x < 4:
+    print("Wstęp jest bezpłatny")
+elif x <= 18:
+    print("Cena biletu: 10zł")
+else:
+    print("Cena biletu: 20zł")
+
+print("Zadanie 2.")
+
