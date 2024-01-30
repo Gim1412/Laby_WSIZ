5982db08686b32c58dd6f377db2f1e752a3f3a3a
GIM1412 <w70568@student.wsiz.edu.pl>
2024-01-30 21:50:01 +0100

    wersja1

---
+++ LAB_3.py
@@ -0,0 +1,23 @@
+# Laboratorium 3
+
+# Zadanie 1
+
+tekst = "Rzeszów jest piękny"
+
+print(tekst[0])
+print(tekst[6], tekst[9], tekst[12],tekst[1])
+
+# Zadanie 2
+
+tekst = "Python jest super"
+
+print(tekst[0])
+print(tekst[-1])
+print(tekst[:: 2])
+print(tekst[1: -1: 3])
+print(tekst[9:])
+print(tekst[::-1])
+
+
+
+
