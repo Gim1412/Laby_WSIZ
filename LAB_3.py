66129bc98a8b16e0bbec4a116b7d4ae9cc2564d3
GIM1412 <w70568@student.wsiz.edu.pl>
2024-01-30 22:29:23 +0100

    wersja1

--- LAB_3.py
+++ LAB_3.py
@@ -18,6 +18,39 @@ print(tekst[1: -1: 3])
 print(tekst[9:])
 print(tekst[::-1])
 
+# Zadanie dodatkowe
+
+imie = input("Podaj swoje imię: ")
+print("Witaj", imie)
+wiek = input("Podaj swój wiek: ")
+print("Twój wiek to: ", wiek, "lat(a)")
+
+imie = input("Podaj swoje imię:")
+nazwisko = input("Podaj swoje nazwisko: ")
+print(imie[0],nazwisko[0])
+
+tekst = input("Wpisz dowolny tekst:")
+
+print(tekst * 5)
+
+tekst = input("Wpisz dowolny tekst:")
+tekst2 = input("Wpisz drugi dowolny tekst:")
+print(tekst + tekst2)
+
+tekst = input("Wpisz dowolny tekst:")
+tekst2 = input("Wpisz drugi dowolny tekst:")
+
+x1 = len(tekst)
+x2 = len(tekst2)
+
+polowa1 = x1 // 2
+polowa2 = x2 // 2
+trzeci = tekst[:polowa1] + tekst2[polowa2:]
+print("Trzeci łańcuch:", trzeci)
+
+
+
+
 
 
 
